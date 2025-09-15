8aae482e113734b59443492d82b22673bf33a7f6
jayjaychukwu <odionye.jude@outlook.com>
2025-09-15 07:22:33 +0100

    make docker functional

--- messaging/src/config/config.js
+++ messaging/src/config/config.js
@@ -1,6 +1,6 @@
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const AUTH_SERVICE_URL = process.env.AUTH_SERVICE_URL;
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 3500;
 export const DB_HOST = process.env.DB_HOST;
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
